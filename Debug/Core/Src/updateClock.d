3787eede57823487574cc70187ca08685f66f4e1
Ty Tran <trandaity151@gmail.com>
2023-10-09 14:10:48 +0700

    generating a hex file for debugging in ex3

--- Debug/Core/Src/updateClock.d
+++ Debug/Core/Src/updateClock.d
@@ -1,4 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/updateClock.o: ../Core/Src/updateClock.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
@@ -23,7 +23,6 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- ../Core/Inc/software_timer.h ../Core/Inc/global.h \
  ../Core/Inc/display7SEG.h ../Core/Inc/updateClock.h ../Core/Inc/global.h
 
 ../Core/Inc/main.h:
@@ -75,10 +74,6 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
-
-../Core/Inc/software_timer.h:
-
-../Core/Inc/global.h:
 
 ../Core/Inc/display7SEG.h:
 
