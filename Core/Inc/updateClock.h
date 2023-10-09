5f1657839d588b214d88d7b7107a2f6a2e507917
Ty Tran <trandaity151@gmail.com>
2023-10-09 14:33:47 +0700

    implement the updateClockBuffer function

--- Core/Inc/updateClock.h
+++ Core/Inc/updateClock.h
@@ -8,8 +8,11 @@
 #ifndef INC_UPDATECLOCK_H_
 #define INC_UPDATECLOCK_H_
 
+#include "main.h"
+#include "display7SEG.h"
 #include "global.h"
 
 void update7SEG(int index);
+void updateClockBuffer();
 
 #endif /* INC_UPDATECLOCK_H_ */
